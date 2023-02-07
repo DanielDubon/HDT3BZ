e7ec30819ed1e9ded60161f7a489c592f56ecc57
DanielDubon <dub22233@uvg.edu.gt>
2023-02-06 21:04:36 -0600

    QuickSort added

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,6 +1,9 @@
 package UI;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
