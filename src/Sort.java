90aeaeeb760db958c865e8930699507ee2f83ea3
Bianca Calderón <98110978+BiancaCalderon@users.noreply.github.com>
2023-02-06 21:35:57 -0600

    Radix Sort

--- src/Sort.java
+++ src/Sort.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class Sort <T> {
     public IComparator myCompare;
@@ -92,5 +94,38 @@ public class Sort <T> {
         quickSort(myArray, inf, i - 1);
         quickSort(myArray, i + 1, sup);
     }
+    public class RadixSort {
+        public static void sort(int[] arr) {
+            final int RADIX = 10;
 
+            List<Integer>[] bucket = new ArrayList[RADIX];
+            for (int i = 0; i < bucket.length; i++) {
+                bucket[i] = new ArrayList<>();
+            }
+
+            boolean maxLength = false;
+            int temp = -1, placement = 1;
+            while (!maxLength) {
+                maxLength = true;
+
+                for (int i : arr) {
+                    temp = i / placement;
+                    bucket[temp % RADIX].add(i);
+                    if (maxLength && temp > 0) {
+                        maxLength = false;
+                    }
+                }
+
+                int a = 0;
+                for (int b = 0; b < RADIX; b++) {
+                    for (int i : bucket[b]) {
+                        arr[a++] = i;
+                    }
+                    bucket[b].clear();
+                }
+
+                placement *= RADIX;
+            }
+        }
+    }
 }
